54ab82bf264feaea355f7d75ae6d2219ff681936
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-02-24 18:13:45 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs.outPath { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          bear
+          clang
+          valgrind
+          gdb
+        ];
+      };
+    };
+}
